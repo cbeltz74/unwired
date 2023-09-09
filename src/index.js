ff08a03ab4e7477d5b47ffc5e0f52e007e70dbe7
Colin Beltz <colinbeltz@gmail.com>
2023-09-09 15:30:47 -0500

    first

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFaNNJ972VXPrQaI3YJoCbsYa4LjmJ97k",
+  authDomain: "notnotion-2e76b.firebaseapp.com",
+  projectId: "notnotion-2e76b",
+  storageBucket: "notnotion-2e76b.appspot.com",
+  messagingSenderId: "524854660386",
+  appId: "1:524854660386:web:526613dd99b9889d5d697b"
+};
+
+
+const app = initializeApp(firebaseConfig);
