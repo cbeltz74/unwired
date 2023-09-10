a2e57c4bfc4f2ec4e1d1e04216f77a2577a6e2c2
Colin Beltz <colinbeltz@gmail.com>
2023-09-10 10:11:22 -0500

    completd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, onSnapshot, addDoc, deleteDoc, doc } from "firebase/firestore";
+import { getFirestore, collection, getDocs, onSnapshot, addDoc, deleteDoc, doc, query, where} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCFaNNJ972VXPrQaI3YJoCbsYa4LjmJ97k",
@@ -13,46 +13,52 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 const colRef = collection(db, "movies");
+const qRef = query(colRef, where("category", "===", "drama"));
 
-getDocs(colRef)
-.then(data => {
-    let movies = [];
-    data.docs.forEach(document => {
-        movies.push({...document.data(), id: document.id})
+
+getDocs(qRef)
+    .then(data => {
+        let movies = [];
+        data.docs.forEach(document => {
+            movies.push({...document.data(), id: document.id});
+        });
+        console.log(movies);
+    })
+    .catch(error => {
+        console.log(error);
     });
-    console.log(movies);
-})
 
-.catch(error => {
-    console.log(error);
-})
 
-// onSnapshot(colRef, () => {
+// onSnapshot(colRef, (data) => {
 //     let movies = [];
-//     data.docs.foreEach(document => {
+//     data.docs.forEach(document => {
 //         movies.push({...document.data(), id: document.id});
 //     });
 //     console.log(movies);
 // });
+
 
 const addForm = document.querySelector(".add");
 addForm.addEventListener("submit", event => {
     event.preventDefault();
     addDoc(colRef, {
         name: addForm.name.value,
-        description: addForm.description.value
+        description: addForm.description.value,
+        category: addForm.category.value
     })
     .then(() => {
         addForm.reset();
     });
 });
 
+
 const deleteForm = document.querySelector(".delete");
 deleteForm.addEventListener("submit", event => {
     event.preventDefault();
     
     const documentReference = doc(db, "movies", deleteForm.id.value);
-    deleteDoc(documentReference).then(() => {
-        deleteForm.reset();
+    deleteDoc(documentReference)
+    .then(() => {
+            deleteForm.reset();
     });
 });
